745e7c6b051ec1a8f0eef51a975aaaf4827afd1b
ctl <ctlchild@163.com>
2019-10-07 00:55:57 +0800

    add hw4

---
+++ hw4/UFO/Assets/Scripts/ScoreRecorder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreRecorder : MonoBehaviour{
+    public int score;                   //分数
+    void Start (){
+        score = 0;
+    }
+    //记录分数
+    public void Record(GameObject disk){
+        int temp = disk.GetComponent<DiskData>().score;
+        score = temp + score;
+    }
+    //重置分数
+    public void Reset(){
+        score = 0;
+    }
+}
